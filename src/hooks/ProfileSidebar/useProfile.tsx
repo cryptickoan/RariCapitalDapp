899667534b6dd93459f5c1466e36efb4f30292f9
cryptickoan <me@cryptickoan.com>
2021-08-03 16:04:22 -0600

    Making hook more flexible :construction:
    Now this hook can be used once per element instead of bulk querying

--- src/hooks/ProfileSidebar/useProfile.tsx
+++ src/hooks/ProfileSidebar/useProfile.tsx
@@ -1,23 +1,14 @@
 import { useRari } from '../../context/RariProvider'
-import { useQueries } from 'react-query'
+import { useQuery } from 'react-query'
 import { Pool, getAccountBalance} from '../../context/PoolProvider'
 
-export const useProfile = () => {
+export const useAccountBalance = (pool: Pool) => {
     const { state } = useRari()
 
-    const queries: {}[] = Object.values(Pool).map((pool) => 
-        (
-            { 
-            queryKey: state.address + " " + pool + " account balance", 
-            queryFn: async () => {
-                const allocation = await getAccountBalance(pool, state.rari, state.address)
-                return allocation
-                }, 
-            refetchOnMount: false 
-            }
-        )
-    )
+    const balance = useQuery(state.address + " " + pool + " account balance", async () => {
+        const balance = await getAccountBalance(pool, state.rari, state.address)
+        return balance
+    })
 
-    const hey = useQueries(queries)
-    return hey
+    return balance
 }
