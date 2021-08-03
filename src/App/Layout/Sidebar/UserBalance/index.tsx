022154b5c14d9f2b92b84ae523cc5cb6ccb85898
cryptickoan <me@cryptickoan.com>
2021-08-03 16:54:16 -0600

    Renaming hook :truck:

--- src/App/Layout/Sidebar/UserBalance/index.tsx
+++ src/App/Layout/Sidebar/UserBalance/index.tsx
@@ -1,5 +1,5 @@
 import { Pool } from "../../../../context/PoolProvider"
-import { useAccountBalance } from "../../../../hooks/ProfileSidebar/useProfile"
+import { useAccountBalance } from "../../../../hooks/YieldAggregator/useAccountBalance"
 import Spinner from "../../../components/Icons/Spinner"
 import InfoBar from "../../../components/InfoBar"
 import { useNavigate } from "react-router"
@@ -18,7 +18,7 @@ const UserBalance = ({pool}: {pool: Pool}) => {
 
     if (typeof balance !== 'undefined' && balance < 1) return null
 
-    const data = [pool.toString(), `$${balance?.toLocaleString()}`]
+    const data = [pool.toString(), `$${balance?.toLocaleString()}`, `$`]
     return (
         <InfoBar data={data} width="100%" height="5%" onClick={toPoolInfo} diffOnHover/>
     )
