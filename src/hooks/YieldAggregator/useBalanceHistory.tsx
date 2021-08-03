e48ad2f4607ac6918f3c7bc0593370d39b8dd980
cryptickoan <me@cryptickoan.com>
2021-08-03 13:32:02 -0600

    Fixing a variable :construction:

--- src/hooks/YieldAggregator/useBalanceHistory.tsx
+++ src/hooks/YieldAggregator/useBalanceHistory.tsx
@@ -4,13 +4,14 @@ import { Pool, getBalanceHistory } from "../../context/PoolProvider";
 
 // Dependencies
 import { useQueries } from "react-query";
+import Rari from "../../sdk/rari-sdk";
 
 // Var
 const blocksPerDay = 6500;
 
 // Function used by query
-const getBalanceHistoryFunc = async (address: string, title: Pool, timerange: string, state: RariState): Promise<{categories: string[], data: number[]}> => {
-    const latestBlock = await state.rari.web3.eth.getBlockNumber()
+export const getBalanceHistoryFunc = async (address: string, title: Pool, timerange: string, rari: Rari): Promise<{categories: string[], data: number[]}> => {
+    const latestBlock = await rari.web3.eth.getBlockNumber()
     console.log(latestBlock)
     
     const blockStart =
@@ -22,7 +23,7 @@ const getBalanceHistoryFunc = async (address: string, title: Pool, timerange: st
         ? latestBlock - blocksPerDay * 10
         : 0;
 
-    const balance = await getBalanceHistory( title, state.rari, address, blockStart)
+    const balance = await getBalanceHistory( title, rari, address, blockStart)
 
     const categories = (balance).map((point: any) => {
       return new Date(point.timestamp * 1000).toLocaleDateString("en-US")
@@ -39,9 +40,9 @@ const getBalanceHistoryFunc = async (address: string, title: Pool, timerange: st
 // Hook
 const useBalanceHistory = (address: string, title: Pool, timerange: string[], state: RariState) => {
   const results: any = useQueries([
-    { queryKey: address + " " + title + " " + timerange[0] + " balance history", queryFn: (): any => getBalanceHistoryFunc(address, title, timerange[0], state), refetchOnMount: false },
-    { queryKey: address + " " + title + " " + timerange[1] + " balance history", queryFn: (): any => getBalanceHistoryFunc(address, title, timerange[1], state),  refetchOnMount: false},
-    { queryKey: address + " " + title + " " + timerange[2] + " balance history", queryFn: (): any => getBalanceHistoryFunc(address, title, timerange[2], state),  refetchOnMount: false}
+    { queryKey: address + " " + title + " " + timerange[0] + " balance history", queryFn: (): any => getBalanceHistoryFunc(address, title, timerange[0], state.rari), refetchOnMount: false },
+    { queryKey: address + " " + title + " " + timerange[1] + " balance history", queryFn: (): any => getBalanceHistoryFunc(address, title, timerange[1], state.rari),  refetchOnMount: false},
+    { queryKey: address + " " + title + " " + timerange[2] + " balance history", queryFn: (): any => getBalanceHistoryFunc(address, title, timerange[2], state.rari),  refetchOnMount: false}
   ])
   return results
 }
