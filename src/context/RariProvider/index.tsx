cd56f418adab30aa88ec6f9314c2c4c62e0ab665
cryptickoan <me@cryptickoan.com>
2021-08-04 23:26:34 -0600

    :zap: Extending redux state for deposit/withdraw action

--- src/context/RariProvider/index.tsx
+++ src/context/RariProvider/index.tsx
@@ -24,8 +24,8 @@ export interface RariContextData {
 export const RariContext = createContext<RariContextData | undefined>(undefined)
 
 // Initial State for RariState
-// 0x0000000000000000000000000000000000000000
-const EmptyAddress = "0x29c89a6cb342756e63a6c78d21adda6290eb5cb1";
+// 0x29c89a6cb342756e63a6c78d21adda6290eb5cb1
+const EmptyAddress = "0x0000000000000000000000000000000000000000";
 const initialState = {rari: new Rari(chooseBestWeb3Provider()), fuse: initFuseWithProviders(),  address: EmptyAddress, isAuthed: false}
 
 export const RariContextProvider = ({children}: {children: ReactNode}) => {
