167aeed22b7fca9d080b91b4b8c8bcae2b754f6c
cryptickoan <me@cryptickoan.com>
2021-08-06 14:03:58 -0600

    :lipstick: Updating props :nail_care:

--- src/App/Apps/Fuse/FuseSinglePool/GraphArea/styles.tsx
+++ src/App/Apps/Fuse/FuseSinglePool/GraphArea/styles.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components"
 
-export const SimulationInput = styled.input`
-width: 90%;
+
+type SimulationInputProps = {
+    width?: string
+}
+
+export const SimulationInput = styled.input<SimulationInputProps>`
+width: ${props => props.width ?? "90%"};
 background-color:${props => props.theme.light ? "white" : "black" };
 color: ${props => props.theme.light ? "black" : "white" };
 box-shadow: ${props => props.theme.light ? "0 0 15px -3px black" : "0 0 10px -6px #F0F0F0" };
