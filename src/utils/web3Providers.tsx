54fd2f0d356d6a5eae8d61ad31436da0a1930fd5
cryptickoan <me@cryptickoan.com>
2021-08-06 14:00:30 -0600

    :bug: Updating turboGethURL
    also removed some unnecessary code

--- src/utils/web3Providers.tsx
+++ src/utils/web3Providers.tsx
@@ -1,35 +1,27 @@
-import Fuse from "../sdk/fuse-sdk/src";
+import Fuse from "../sdk/fuse-sdk"
 
+export const infuraURL = `https://mainnet.infura.io/v3/bd74c68478354521921a1ac57614862b`;
 export const turboGethURL = `https://eth-mainnet.alchemyapi.io/v2/2Mt-6brbJvTA4w9cpiDtnbTo6qOoySnN`;
 
-// Types
-declare global {
-    interface Window {
-        ethereum: any,
-        web3: any
-    }
-}
-
 export function chooseBestWeb3Provider() {
-    if (typeof window === "undefined") {
-        return turboGethURL
-    }
+  if (typeof window === "undefined") {
+    return turboGethURL;
+  }
 
-
-    if(window.ethereum) {
-        return window.ethereum
-    } else if (window.web3) {
-        return window.web3.currentProvider
-    } else {
-        return turboGethURL
-    }
+  if (window.ethereum) {
+    return window.ethereum;
+  } else if (window.web3) {
+    return window.web3.currentProvider;
+  } else {
+    return turboGethURL;
+  }
 }
 
 export const initFuseWithProviders = (provider = chooseBestWeb3Provider()) => {
-    const fuse = new Fuse(provider)
+  const fuse = new Fuse(provider);
 
-    // @ts-ignore
-    fuse.contracts.FusePoolLens.setProvider(turboGethURL)
+  // @ts-ignore We have to do this to avoid Infura ratelimits on our large calls.
+  fuse.contracts.FusePoolLens.setProvider(turboGethURL);
 
-    return fuse
-}
+  return fuse;
+};
