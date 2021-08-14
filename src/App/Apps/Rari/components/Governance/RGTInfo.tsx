cfec1cf5d9f015ce48ace4f24b5e31be97dea4d0
cryptickoan <me@cryptickoan.com>
2021-08-14 00:17:46 -0600

    :sparkles: Adding some extra information :sparkles:
    Also worked on sizing components

--- src/App/Apps/Rari/components/Governance/RGTInfo.tsx
+++ src/App/Apps/Rari/components/Governance/RGTInfo.tsx
@@ -8,12 +8,24 @@ import { SpacingContainer, StyledP, Card } from '../../../../Shared'
 import Spinner from '../../../../Shared/Icons/Spinner'
 
 const RGTInfo = () => {
-    const {RGTPrice, RGTSupply, RGTVol} = useRGTInfo()
+    const {RGTPrice, RGTSupply, RGTVol, RGTLow, RGTHigh} = useRGTInfo()
+
      return (
-        <SpacingContainer direction="column" height="100%" justifyContent="space-evenly">
-            <StyledP>Rari Governance Token Overview</StyledP>
-            <SpacingContainer  width="90%" justifyContent="space-evenly">
-                <Card flexBasis="25%" height="5vw" borderRadius="15px" direction="column"> 
+         <>
+        <SpacingContainer height="10%" margin="3% 0 0 0">
+            <StyledP size="1vw" opacity="0.4"><strong>Rari Governance Token Overview</strong></StyledP>
+        </SpacingContainer>
+        <SpacingContainer direction="column" height="80%" justifyContent="space-evenly">
+            <SpacingContainer height="50%"  width="90%" justifyContent="space-evenly">
+                <Card flexBasis="25%" height="50%" borderRadius="15px" direction="column"> 
+                    {!RGTLow ? <Spinner /> :  
+                        <>
+                            <StyledP size="1.5vw" separate="1.6vw" glow>${ RGTLow }</StyledP>
+                            <StyledP size="0.8vw" separate="0.9vw">24h Low</StyledP>
+                        </>
+                    }
+                </Card>
+                <Card flexBasis="25%" height="50%" borderRadius="15px" direction="column"> 
                     {!RGTPrice ? <Spinner /> :  
                         <>
                             <StyledP size="1.5vw" separate="1.6vw" glow>${ RGTPrice.substring(0, 5) }</StyledP>
@@ -21,7 +33,27 @@ const RGTInfo = () => {
                         </>
                     }
                 </Card>
-                <Card flexBasis="40%" height="5vw" direction="column" borderRadius="15px">
+                <Card flexBasis="25%" height="50%" borderRadius="15px" direction="column"> 
+                    {!RGTHigh ? <Spinner /> :  
+                        <>
+                            <StyledP size="1.5vw" separate="1.6vw" glow>${ RGTHigh }</StyledP>
+                            <StyledP size="0.8vw" separate="0.9vw">24h High</StyledP>
+                        </>
+                    }
+                </Card>
+                
+            </SpacingContainer>
+
+            <SpacingContainer width="90%" height="50%" justifyContent="space-evenly" alignItems="flex-start"> 
+                <Card flexBasis="40%" height="60%" direction="column" borderRadius="15px">
+                    {!RGTVol ? <Spinner /> :  
+                            <>
+                                <StyledP size="1.5vw" separate="1.6vw" glow>${ RGTVol.toLocaleString() }</StyledP>
+                                <StyledP size="0.8vw" separate="0.9vw">Trading Volume</StyledP>
+                            </>
+                        }
+                </Card>
+                <Card flexBasis="40%" height="60%" direction="column" borderRadius="15px">
                  {!RGTSupply ? <Spinner /> :  
                         <>
                             <StyledP size="1.5vw" separate="1.6vw" glow>{ RGTSupply.toLocaleString() }</StyledP>
@@ -30,17 +62,8 @@ const RGTInfo = () => {
                     }
                 </Card> 
             </SpacingContainer>
-            <SpacingContainer flexBasis="50%" width="90%" height="50%" justifyContent="space-evenly">
-                <Card flexBasis="40%" height="5vw" direction="column" borderRadius="15px">
-                    {!RGTVol ? <Spinner /> :  
-                            <>
-                                <StyledP size="1.5vw" separate="1.6vw" glow>${ RGTVol.toLocaleString() }</StyledP>
-                                <StyledP size="0.8vw" separate="0.9vw">Trading Volume</StyledP>
-                            </>
-                        }
-                </Card>
-            </SpacingContainer>
         </SpacingContainer>
+        </>
     )
 }
 
