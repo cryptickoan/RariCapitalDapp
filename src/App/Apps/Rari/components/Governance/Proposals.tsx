37ca21ecd76dcac9adc1b36c0c164195a059863c
cryptickoan <me@cryptickoan.com>
2021-08-14 00:13:40 -0600

    :lipstick: Styling and sizing :nail_care:

--- src/App/Apps/Rari/components/Governance/Proposals.tsx
+++ src/App/Apps/Rari/components/Governance/Proposals.tsx
@@ -18,8 +18,8 @@ const Proposals = () => {
     const Snap: {[key: string]: []} = useGovernance()
     return (
         <>
-        <StyledP size="1vw" opacity="0.8" margin="2% 0 2% 0"><strong>Recent Proposals</strong></StyledP>
-        <SpacingContainer height="80%" padding="1%" maxHeight="20vw" direction="column" justifyContent="flex-start" overflowY="scroll">
+        <SpacingContainer height="100%" maxHeight="22vw" margin="2% 0 2% 0" direction="column" justifyContent="flex-start" overflowY="scroll">
+            <StyledP size="1vw" opacity="0.4" margin="2% 0 2% 0"><strong>Recent Proposals</strong></StyledP>
             { !Snap ?  <Spinner/> : Snap.proposals.map((proposal: any) => proposal.title === 'Hello World' ? null : <Proposal proposal={proposal}/>) }
         </SpacingContainer>
         </>
@@ -37,15 +37,15 @@ const Proposal = ({proposal}: any) => {
                 <Tooltip id="tooltip-top">Click to visit proposal</Tooltip>
             }        
         >
-            <ProposalContainer direction="column" active={proposal.state} margin="0 0 4% 0" width="80%">
-                <a href={`https://snapshot.org/#/rari/proposal/${proposal.id}`} target="_blank" rel="noreferrer">
-                <StyledP size="0.9vw" glow><strong>{proposal.title}</strong></StyledP>
+            <a id="proposal" href={`https://snapshot.org/#/rari/proposal/${proposal.id}`} target="_blank" rel="noreferrer">
+            <ProposalContainer direction="column" active={proposal.state} margin="0 0 4% 0" width="80%" padding="3% 5%">
+                <StyledP size="0.9vw" separate="1.5vw"><strong>{proposal.title}</strong></StyledP>
                 <StyledP size="0.9vw" textAlign="justify" neuropolNova separate="1vw">{removeMd((proposal.body).slice(0,150)).replace('Summary', '')}...</StyledP>
-                <SpacingContainer margin="10px">
+                <SpacingContainer margin="10px 0 0 0">
                     <StatusDiv active={proposal.state}>{proposal.state}</StatusDiv>
                 </SpacingContainer>
-                </a>
             </ProposalContainer>
+            </a>
         </OverlayTrigger>
     )
 }
