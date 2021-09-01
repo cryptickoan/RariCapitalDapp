15c96918baef01ce092214aa99f1cbbe62dc4f38
cryptickoan <me@cryptickoan.com>
2021-08-31 23:35:22 -0600

    Centering loading spinner

--- src/App/Apps/Rari/components/Governance/Proposals.tsx
+++ src/App/Apps/Rari/components/Governance/Proposals.tsx
@@ -20,7 +20,7 @@ const Proposals = () => {
         <>
         <SpacingContainer height="100%" maxHeight="22vw" margin="2% 0 2% 0" direction="column" justifyContent="flex-start" overflowY="scroll">
             <StyledP size="1vw" opacity="0.4" margin="2% 0 2% 0"><strong>Recent Proposals</strong></StyledP>
-            { !Snap ?  <Spinner/> : Snap.proposals.map((proposal: any) => proposal.title === 'Hello World' ? null : <Proposal proposal={proposal}/>) }
+            { !Snap ?( <SpacingContainer> <Spinner/> </SpacingContainer> ): Snap.proposals.map((proposal: any) => proposal.title === 'Hello World' ? null : <Proposal proposal={proposal}/>) }
         </SpacingContainer>
         </>
     )
